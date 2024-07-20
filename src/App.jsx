ae7aaff0252ff0519ca11302ec9d34e639076b7f
Muhammadbijjar <Muhammadbijjar2@gmail.com>
2024-07-20 08:10:49 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Quiz from './components/Quiz'
+
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <Quiz/>
+    </>
+  )
+}
+
+export default App
